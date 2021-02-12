57de8ce39963dfffc5369168381c21dfb8aa5260
Chris Burgess <9002722+cgburgess@users.noreply.github.com>
2021-02-12 12:04:28 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# 426-coding-challenge
+
+Read a file of text, determine the n most frequently used words, and print out a sorted list of those words along with their frequencies.
---
+++ wordcount.js
@@ -0,0 +1,11 @@
+const sherlock = require('fs').readFileSync(process.argv[2] || 'sherlock.txt').toString()
+const n = 10
+
+/* write your code here to solve the problem stated in the README
+ * Reading in the file and determining n (10) is already done for you!
+ * ---
+ * Read a file of text, determine the n most frequently used words,
+ * and print out a sorted list of those words along with their frequencies.
+ */
+
+console.log(top_n);
